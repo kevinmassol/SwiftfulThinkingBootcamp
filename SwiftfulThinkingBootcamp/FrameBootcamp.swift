5bd19a91de9abad0fa459f0e52c0123c99643ec5
Kevin Massol <kevin.massol@icloud.com>
2025-11-15 10:20:34 +0100

    🚀 feat: add frames and change their alignments

---
+++ SwiftfulThinkingBootcamp/FrameBootcamp.swift
@@ -0,0 +1,29 @@
+//
+//  FrameBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by KEVIN on 15/11/2025.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.red)
+            .frame(height: 100, alignment: .top)
+            .background(Color.orange)
+            .frame(width: 150)
+            .background(Color.purple)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.pink)
+            .frame(height: 400)
+            .background(Color.green)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color.yellow)
+    }
+}
+
+#Preview {
+    FrameBootcamp()
+}
